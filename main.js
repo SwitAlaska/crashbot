ac52397d7cd2d4d42a6af147a4fb034d81d65414
SwitAlaska <47485362+SwitAlaska@users.noreply.github.com>
2020-01-19 20:24:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var request = require('request');
 var uniqid = require('uniqid');
 
-async function test(){
+async function send(){
     var i = 0;
-    while(i < 4){
+    while(i < 50000){
 
         var defautData = uniqid();
-        var mail = "Attackeedt" + i + "@hotmail.com";
+        var mail = "attack" + i + "@gmail.com";
         var Birthday = "09071982";
 
         await sendData(defautData,mail,Birthday);
@@ -15,16 +15,16 @@ async function test(){
 }
 
 
-setInterval(test, 40000);
+setInterval(send, 40000);
 
 async function sendData(defautData,mail,Birthday){
     request.post({
       headers: {'content-type' : 'application/x-www-form-urlencoded'},
-      url:     'http://daqop.ddns.net:80',
-      form:    { sumbit: "heydude",username:defautData,birthday:Birthday,Password: defautData,email:mail}
+      url:     'http://uri.com:80',
+      form:    { sumbit: "attack",username:defautData,birthday:Birthday,Password: defautData,email:mail}
     }, function(error, response, body){
         if(!error) {
-            console.log("compte créer identifiant = " + defautData);
+            console.log("Done. " + defautData);
         }
         else {
             console.log(error);
